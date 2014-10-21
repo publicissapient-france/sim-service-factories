bd44b987b4ec59bd85b81b95b8f36887bcd4276b
RomainN <romain.niveau@gmail.com>
2014-10-21 18:06:55 +0200

    factory pour javascript

---
+++ factory.js
@@ -0,0 +1,6 @@
+var vertx     = require('vertx');
+var container = require('vertx/container');
+var console   = require('vertx/console');
+var utils     = require('./utils.js');
+
+var id = utils.uuid();
