87c2b6893d8beaaab145ecc13b9f46678c3a6949
RomainN <romain.niveau@gmail.com>
2014-12-22 15:56:41 +0100

    Squelette avec hello et achat

--- factory.groovy
+++ factory.groovy
@@ -2,8 +2,48 @@ import org.vertx.groovy.core.eventbus.Message
 import org.vertx.groovy.core.Vertx
 
 Vertx vx = vertx
-def id = "factory-"+UUID.randomUUID().toString()
+def logger = container.logger
 
+def id = "factory-" + UUID.randomUUID().toString()
 
+// Say hello every 1 second to the city
+vx.setPeriodic(1000) { timerID ->
+    logger.info("Say hello");
+    vx.eventBus.publish("/city", [
+            action: "hello",
+            from: id,
+            team: "your-team-name",
+            type: "factory",
+            version: "1.0"
+    ]);
+}
 
+// Listen message on private address
+vx.eventBus.registerHandler("/city/factory/" + id) { message ->
+    logger.info("Get a message from " + message.body.from);
+    logger.info("${message.body}");
+    switch (message.body.action) {
+    // Farm proposes an offer
+        case "response":
+            // Replies to the offer
+            message.reply([
+                    action: "acquittement",
+                    from: id,
+                    quantity: message.body.quantity
+            ]);
+            break;
+        case "purchase":
+            logger.info("Bank accepts a transaction");
+            // Here manage your stock
+            break;
+    }
+}
 
+// Request hop from farms
+vx.setTimer(2000) { timerId ->
+    vx.eventBus.publish("/city/farm", [
+            action: "request",
+            from: id,
+            quantity: 10
+    ]);
+}
